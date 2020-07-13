e8caf72509449ae83c96fe78eec941ab540226f9
Tatiana <tatianaw83@gmail.com>
2020-07-12 19:30:52 -0700

    Updates

---
+++ precache-manifest.7dfe5f6eac86facb4cefc8d92ad2378f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa893531ddaa2902c0dc72c82b728c14",
+    "url": "/react-gameofchance/index.html"
+  },
+  {
+    "revision": "1a198615c08c371f1144",
+    "url": "/react-gameofchance/static/css/main.7ec7d247.chunk.css"
+  },
+  {
+    "revision": "44718a8ba93fbefa41f5",
+    "url": "/react-gameofchance/static/js/2.94316aef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gameofchance/static/js/2.94316aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a198615c08c371f1144",
+    "url": "/react-gameofchance/static/js/main.f5357ac8.chunk.js"
+  },
+  {
+    "revision": "8fb98ecf6f72ce2c25fe",
+    "url": "/react-gameofchance/static/js/runtime-main.038e7e60.js"
+  },
+  {
+    "revision": "edf254eea0ca51397e883b6908ad57cb",
+    "url": "/react-gameofchance/static/media/pixel-coin-png-4.edf254ee.gif"
+  }
+]);
